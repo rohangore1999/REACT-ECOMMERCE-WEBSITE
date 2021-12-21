07a2d612aeb16cccde9cd2aee694c5a347a93d80
Rohan Gore <gorerohan15@gmail.com>
2021-12-21 21:52:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ import {
   Route
 } from "react-router-dom";
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import { useEffect } from 'react';
 
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import EditIcon from '@material-ui/icons/Edit';
+import EmailIcon from '@material-ui/icons/Email';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+
+import React, { useState, useEffect } from 'react';
+
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 
 function App() {
 
@@ -27,6 +35,47 @@ function App() {
     }, [])
     return (null)
   }
+
+
+
+  const toggleDrawer = () => {
+    setState(true)
+  }
+
+  const toggleWhatsapp = () => {
+    window.open('https://api.whatsapp.com/send?phone=919762228932&text=Hi.')
+    handleClose()
+
+  }
+
+  const toggleEmail = () => {
+    window.open('mailto:')
+    handleClose()
+  }
+
+    // check toggle state [boolean]
+    const [state, setState] = useState(false)
+
+  const actions = [
+    { icon: <EmailIcon onClick={toggleEmail} />, name: 'Email' },
+    { icon: <WhatsAppIcon onClick={toggleWhatsapp} />, name: 'Whatsapp' },
+  ];
+
+  const style = {
+    margin: 0,
+    right: 20,
+    bottom: 20,
+    position: 'fixed',
+  
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
 
   return (
@@ -58,6 +107,29 @@ function App() {
       {/* <ProductList/> */}
       {/* <Product/> */}
       {/* <Cart /> */}
+
+
+
+
+      <SpeedDial 
+        ariaLabel="SpeedDial openIcon example"
+        style={style}
+        hidden={false}
+        icon={<QuestionAnswerIcon openicon={<EditIcon />} />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction style={{backgroundColor: "#323232"}}
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={handleClose}
+            href={action.link}
+          />
+        ))}
+      </SpeedDial>
     </div>
   );
 }
